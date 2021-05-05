732017a104ac7a5ed471f115adbbec59843afca1
Monty Sinha <monty.sinha2013@live.co.uk>
2021-05-05 11:26:31 +0100

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ParanormalFacts.com.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult Werewolf()
+        {
+            return View();
+        }
+        public ActionResult Vampires()
+        {
+            return View();
+        }
+        public ActionResult UFOs()
+        {
+            return View();
+        }
+
+    }
+}
